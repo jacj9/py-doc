9cf3ebc13fa3bb1035afae553a4f8cb30ea1362a
jacj9 <jjoapr@gmail.com>
2025-01-20 14:32:36 -0800

    Add files via upload

---
+++ xt_hello.py
@@ -0,0 +1,26 @@
+"""Documenting simple python exercises.
+Written on: January 20, 2025"""
+
+
+## #############
+iname = input("Enter your name: ")
+
+def hello_world():
+    print("Hello World!")
+
+def hello(name):
+    print(f"Hello, {name}!")
+
+hello_world()
+hello(iname)
+
+
+## ############################
+def say_hello():
+    print("Hello!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+say_hello()
+greet("Tom")
