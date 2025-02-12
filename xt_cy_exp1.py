df769b33b53da1bf9279a1734093f3d3aa100f91
jacj9 <jjoapr@gmail.com>
2025-02-11 21:40:54 -0800

    Add files via upload

--- xt_cy_exp1.py
+++ xt_cy_exp1.py
@@ -455,30 +455,45 @@ Written on: January 26, 2025"""
 # use the "Have I Been Pwned" API (https://haveibeenpwned.com/API/v3) to check if a 
 # password has been leaked.
 ######################################################
+# import requests
+# import hashlib
+
+# # Read the file containing usernames and passwords
+# with open('passwords.txt', 'r') as f:
+#     for line in f:
+#         # Split the line into username and passord
+#         username, password = line.strip().split(',')
+
+#         # Hash the password using SHA-1 algorith
+#         password_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+
+#         # Make a request to "Have I Been Pwned" API to check if the password has been leaked
+#         response = requests.get(f'https://api.pwnedpasswords.com/range/{password_hash[:5]}')
+
+#         # If the response status code is 200, it means the password has been leaked
+#         if response.status_code == 200:
+#             # Get the list of hashes of leaked passwords that start with the same 5 character as the input password
+#             hashes = [line.split(':') for line in response.text.splitlines()]
+
+#             # Check if the hash of the input password matches any of the leaked password hashes
+#             for h, count in hashes:
+#                 if password_hash[5:] == h:
+#                     print(f"Password for user {username} has been leaked {count} times.")
+#                     break
+#         else:
+#             print(f"Could not check password for user {username}.")
+
+
+##########################################
+# Practice the above exercise on my own
+# Written on: February 9, 2025
+#########################################
+
 import requests
 import hashlib
 
-# Read the file containing usernames and passwords
 with open('passwords.txt', 'r') as f:
     for line in f:
-        # Split the line into username and passord
-        username, password = line.strip().split(',')
+        text = f.split().seperate(',')
 
-        # Hash the password using SHA-1 algorith
-        password_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-
-        # Make a request to "Have I Been Pwned" API to check if the password has been leaked
-        response = requests.get(f'https://api.pwnedpasswords.com/range/{password_hash[:5]}')
-
-        # If the response status code is 200, it means the password has been leaked
-        if response.status_code == 200:
-            # Get the list of hashes of leaked passwords that start with the same 5 character as the input password
-            hashes = [line.split(':') for line in response.text.splitlines()]
-
-            # Check if the hash of the input password matches any of the leaked password hashes
-            for h, count in hashes:
-                if password_hash[5:] == h:
-                    print(f"Password for user {username} has been leaked {count} times.")
-                    break
-        else:
-            print(f"Could not check password for user {username}.")
+        hash_password = hashlib.sha256(f).hexdigest()
