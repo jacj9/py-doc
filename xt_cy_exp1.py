3f90fbe8b55203a0d4e89813b1e88bbde93008e0
jacj9 <jjoapr@gmail.com>
2025-02-16 21:25:21 -0800

    Add files via upload

--- xt_cy_exp1.py
+++ xt_cy_exp1.py
@@ -571,3 +571,60 @@ Written on: January 26, 2025"""
 # print(check_password_strength(password1))
 # print(check_password_strength(password2))
 # print(check_password_strength(password3))
+
+
+###########################################################
+# 7. Write a Python program that creates a password strength meter. The program should prompt 
+# the user to enter a password and check its strength based on criteria such as length, complexity, 
+# and randomness. Afterwards, the program should provide suggestions for improving the password's 
+# strength.
+
+# The highest strength password that meets the criteria specified in the program will receive 
+# a score of 5.
+# (e.g. at least 8 characters, contains both uppercase and lowercase letters, and at least 
+# one number and one special character).
+
+# Written on: February 17, 2025
+################################################################
+
+## Sample Solution
+
+import re
+
+def check_password_strength(password):
+    score = 0
+    suggestions = []
+
+    # check length
+    if len(password) >= 8:
+        score += 1
+    else:
+        suggestions.append("Password should be at least 8 characters long")
+
+    # check for uppercase letter
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        suggestions.append("Password should contain at least one uppercase letter")
+
+    # check for lowercase letter
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        suggestions.append("Password should contain at least one lowercase letter")
+
+    # check for numeric digit
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        suggestions.append("Password should contain at least one numeric digit")
+
+    # check for special character
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+    else:
+        suggestions.append("Password should contain at least one special character (!@#$%^&*(),.?\":{}|<>)")
+
+    return score, suggestions
+password = input("Input a password: ")
+print(check_password_strength(password))
