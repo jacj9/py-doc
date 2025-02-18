ee3918f2d134f701b01f0bcb25f0a0f7b34be270
jacj9 <jjoapr@gmail.com>
2025-02-17 20:42:23 -0800

    Add files via upload

--- xt_cy_exp1.py
+++ xt_cy_exp1.py
@@ -587,44 +587,96 @@ Written on: January 26, 2025"""
 # Written on: February 17, 2025
 ################################################################
 
-## Sample Solution
-
+###################################
+## Practice Exercise
+## Written on: February 17, 2025
+###################################
 import re
-
 def check_password_strength(password):
     score = 0
     suggestions = []
-
-    # check length
-    if len(password) >= 8:
+    password_length = len(password)
+      
+    # check for password length
+    if password_length >= 8:
         score += 1
     else:
         suggestions.append("Password should be at least 8 characters long")
 
     # check for uppercase letter
-    if re.search(r"[A-Z]", password):
+    if re.search(r'[A-Z]', password):
         score += 1
     else:
-        suggestions.append("Password should contain at least one uppercase letter")
+        suggestions.append("Password should contain at lease one uppercase letter")
 
-    # check for lowercase letter
-    if re.search(r"[a-z]", password):
-        score += 1
+    #check for lowercase letter
+    if re.search(r'[a-z]', password):
+               score += 1
     else:
         suggestions.append("Password should contain at least one lowercase letter")
 
     # check for numeric digit
-    if re.search(r"\d", password):
+    if re.search(r'\d', password):
         score += 1
     else:
-        suggestions.append("Password should contain at least one numeric digit")
+        suggestions.append("Password should contain a numeric digit")
 
-    # check for special character
-    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    # check for special characters
+    if re.search(r'[!@#$%^&*()_+]', password):
         score += 1
     else:
-        suggestions.append("Password should contain at least one special character (!@#$%^&*(),.?\":{}|<>)")
-
+        suggestions.append("Password should contain at lease one  special character")
+    
+    if score == 5:
+         suggestions.append("Strong password")
     return score, suggestions
-password = input("Input a password: ")
+
+password = 'password1234'
+password2 = 'Pass123#$$%'
+password3 = '#n23K'
 print(check_password_strength(password))
+print(check_password_strength(password2))
+print(check_password_strength(password3))
+
+
+## Sample Solution
+
+# import re
+
+# def check_password_strength(password):
+#     score = 0
+#     suggestions = []
+
+#     # check length
+#     if len(password) >= 8:
+#         score += 1
+#     else:
+#         suggestions.append("Password should be at least 8 characters long")
+
+#     # check for uppercase letter
+#     if re.search(r"[A-Z]", password):
+#         score += 1
+#     else:
+#         suggestions.append("Password should contain at least one uppercase letter")
+
+#     # check for lowercase letter
+#     if re.search(r"[a-z]", password):
+#         score += 1
+#     else:
+#         suggestions.append("Password should contain at least one lowercase letter")
+
+#     # check for numeric digit
+#     if re.search(r"\d", password):
+#         score += 1
+#     else:
+#         suggestions.append("Password should contain at least one numeric digit")
+
+#     # check for special character
+#     if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+#         score += 1
+#     else:
+#         suggestions.append("Password should contain at least one special character (!@#$%^&*(),.?\":{}|<>)")
+
+#     return score, suggestions
+# password = input("Input a password: ")
+# print(check_password_strength(password))
