1fd3327ce723877ecdeb2bc5dd584c27f7ecbbfb
jacj9 <jjoapr@gmail.com>
2025-02-23 21:34:05 -0800

    Add files via upload

--- xt_cy_exp1.py
+++ xt_cy_exp1.py
@@ -716,37 +716,55 @@ Written on: January 26, 2025"""
 # using a random combination of words from a dictionary file.
 # Written on: February 21, 2025
 ########################################################
-import random
 
-# Open the dictionary file and read its content. You can use open() and read()
-with open('passwords2.txt', 'r') as f:
-    for line in f:
-        # Split the content into individual words. You can use the split() method.
-        my_list = line.strip() + "-"
-        random_words = random.sample(my_list, 3)
-        concat = my_list + random_words
-        # print(random_words)
-        # print(my_list, random_words)
-        # for r in my_list:
-        #     print(r)
-        result = "-".join(random_words)
-        print(result)
+## Answer key: Sample solution
+# import random
+
+# def generate_password():
+#     with open('passwords2.txt') as f:
+
+#         words = f.read().splitlines()
         
+#     password = random.sample(words, 4)
 
-# Randomly select a certain number of words from the list. import random
-        # random_words = random.sample(my_list, 5)
-        # print(random_words)
+#     password = "-".join(password)
+    
+#     return password
 
-# Concatenate the selected words together to generate the password. join()
-# result = "-".join(mylist, random_words)
+# password = generate_password()
+# print(password)
 
-# print or return the generated password
+##################################################
+# Practice exercise 8
+# February 22, 2025
+##################################################
+
+# import random
+
+# # Open the dictionary file and read its content. You can use open() and read()
+# with open('passwords2.txt', 'r') as f:
+#     for line in f:
+#         # Split the content into individual words. You can use the split() method.
+#         my_list = f.read().strip() + "-"
+#         random_words = random.sample(my_list, 4)
+#         # print(random_words)
+#         # print(my_list, random_words)
+#         # for r in my_list:
+#         #     print(r)
+#         result = my_list.join(random_words)
+#         print(result)
+
+
+#########################################
+# Practice exercise 8
+# February 23, 2025
+#########################################
 
 import random
 
 def generate_password():
     with open('passwords2.txt') as f:
-        words = f.read().splitline()
+        words = f.read().splitlines()
 
     password = random.sample(words, 4)
 
@@ -754,6 +772,5 @@ def generate_password():
 
     return password
 
-print(generate_password())
-
-    
+password = generate_password()
+print(password)
